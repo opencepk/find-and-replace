031365989b1a772d198be29df6590add776dceeb
hminaee-tc <hminaee@tucows.com>
2024-06-12 18:26:26 -0300

    feat/intial-v initial code

--- setup.py
+++ setup.py
@@ -1,3 +1,19 @@
+# from setuptools import setup
+
+# setup()
+
+
 from setuptools import setup
 
-setup()
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='your-package-name',
+    version='0.1',
+    description='Your package description',
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
